601c5243c50f5c660f8d14fb895e29263740e440
Dong-Toan2004 <149890821+Dong-Toan2004@users.noreply.github.com>
2025-07-07 19:53:15 +0700

    Add project files.

---
+++ Learn_Asp.NetCore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Learn_Asp.NetCore.MvcApplication" Language="C#" %>
---
+++ Learn_Asp.NetCore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learn_Asp.NetCore.Startup))]
+namespace Learn_Asp.NetCore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
